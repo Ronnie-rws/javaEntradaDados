04c83f4caee4d69f6f9904d1fd8f5d9aeed3f188
ronnie <ronnie.willyams.rws@gmail.com>
2022-05-23 16:46:37 -0300

    Criando uma entrada de dados.

--- javaEntradaDados/src/rws/LojaCarros.java
+++ javaEntradaDados/src/rws/LojaCarros.java
@@ -1,8 +1,23 @@
 package rws;
 
+import javax.swing.JOptionPane;
+
 public class LojaCarros {
 	
 	public static void main(String[] args) {
+		
+		
+		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros? ");
+		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas? ");
+		
+		double carroNumero = Double.parseDouble(carros);
+		double pessoaNumero = Double.parseDouble(pessoas);
+		
+		int divisao = (int) (carroNumero / pessoaNumero);
+		
+		double resto = carroNumero % pessoaNumero;
+		
+		System.out.println("A divisão de pessoa deu: " + divisao + " carros " + resto);
 		
 	}
 
